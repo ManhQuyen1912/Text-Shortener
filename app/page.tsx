8d9cdffad3ca085bdaa5794bb72a3c1c77a3eab7
ManhQuyen1912 <nmquyen1912@gmail.com>
2024-08-14 10:03:40 +0700

    add variable to store data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  // 1. Storing the text that the user will enter in the textarea -> String
+  const [text, setText] = useState("");
+  // 2. Saving the summary that the AI API will send us -> String
+  const [summary, setSummary] = useState("");
+  // 3. For storing the current state of the app -> Boolean
+  const [loading, setLoading] = useState(false);
   return (
     <div className="mx-auto w-full max-w-lg py-24 space-y-24 min-h-screen flex flex-col justify-center">
       <div className="space-y-6 w-full text-center">
